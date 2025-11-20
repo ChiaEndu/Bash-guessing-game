35bc77612e59c15fbaabbb7b5b669c14890e3213
Endurance <endurancechia@gmail.com>
2025-11-20 18:03:12 +0100

    corrected (-r) backlash on script.sh

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ x=9
 echo "==============================================="
 echo "Guess a number to win between 0 to 15:"
 echo "==============================================="
-read -p "Enter the value you guess:" y
+read -r -p "Enter the value you guess:" y
 if [[ $y -eq $x ]]; then 
 echo "You Win"
 elif [[ $y -lt $x ]]; then
