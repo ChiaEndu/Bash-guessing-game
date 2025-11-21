16c8d601ab46bd6c0f2e18797034402f9e3d3643
Endurance <endurancechia@gmail.com>
2025-11-21 09:15:28 +0100

    updated guessing game in script.sh and added while  loop

--- script.sh
+++ script.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
-#X=$(( 0 + RANDOM % (15- 0 + 1) ))
-x=9
-echo "==============================================="
-echo "Guess a number to win between 0 to 15:"
-echo "==============================================="
-read -r -p "Enter the value you guess:" y
-if [[ $y -eq $x ]]; then 
-echo "You Win"
-elif [[ $y -lt $x ]]; then
-echo "================================================="
-echo "You loss, below winning Number"
-echo "================================================="
-elif [[ $y -gt $x ]]; then
-echo "================================================="
-echo "You loss, above Winning Number"
-echo "================================================="
-else
-echo "Invalid number, not within guessing range"
-fi
+while true; do
+  # generate random number between 0 and 15
+  x=$(( RANDOM % 16 ))
 
+  echo "==============================================="
+  echo "Guess a number to win between 0 to 15:"
+  echo "==============================================="
+  read -r -p "Enter the value you guess: " y
+
+  if [[ $y -eq $x ]]; then 
+      echo "You Win "
+  elif [[ $y -lt $x ]]; then
+      echo "================================================="
+      echo "You lost. Your guess is below the winning number."
+      echo "================================================="
+  elif [[ $y -gt $x ]]; then
+      echo "================================================="
+      echo "You lost. Your guess is above the winning number."
+      echo "================================================="
+  else
+      echo "Invalid number, not within guessing range."
+  fi
+
+  # ask user if they want to play again
+  read -r -p "Do you want to play again? (y/n): " choice
+  if [[ $choice != "y" ]]; then
+      echo "Thanks for playing "
+      break
+  fi
+done
